ef4c6874ae5d53a114e07a3af6951e938003e841
naela-git <naelaeng83@gmail.com>
2022-03-10 03:41:24 -0800

    add comment to vendors migration file

--- database/migrations/2022_03_10_102335_create_vendors_table.php
+++ database/migrations/2022_03_10_102335_create_vendors_table.php
@@ -25,10 +25,12 @@ class CreateVendorsTable extends Migration
             $table->string('zipcode', 50)->nullable();
             $table->integer('is_active')->default('0');
             $table->integer('created_by')->default('0');
+            // add foreign key  from vendors table to users  and products tables 
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('product_id');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
