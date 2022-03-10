f7b9d76509efd31260d6df0147dbf988ed1e3e4e
mahmoud0210 <mahmoud.mohammad.19982000@gmail.com>
2022-03-10 10:59:33 +0100

    edit route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('products',ProductController::Class);
+Route::resource('products',ProductController::class);
